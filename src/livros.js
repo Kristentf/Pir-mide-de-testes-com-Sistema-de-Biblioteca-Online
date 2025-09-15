11fc04e3802b976f0372d4b172f1d4bc71b3a458
Kristentf <kristentf2@gmail.com>
2025-09-15 20:14:09 -0300

    Arquivos add

---
+++ src/livros.js
@@ -0,0 +1,15 @@
+ export class Livro {
+    constructor(titulo) {
+        this.titulo = titulo;
+        this.disponivel = true;
+    }
+    emprestar() {
+        if (!this.disponivel) {
+            throw new Error("Livro já emprestado!");
+        }
+        this.disponivel = false;
+    }
+    devolver() {
+        this.disponivel = true;
+    }
+}
